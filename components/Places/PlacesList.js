dfc5f330b7be720f7d970b1ff6cc923e1045a796
Wasim Hyder <wasim@creativefaze.com>
2024-02-12 21:33:53 +0500

    Initial screens

---
+++ components/Places/PlacesList.js
@@ -0,0 +1,28 @@
+import { FlatList, View, Text, StyleSheet } from 'react-native';
+import PlaceItem from './PlaceItem';
+
+function PlacesList({ places }) {
+  if (!places || places.length === 0) {
+    return <View style={styles.fallbackContainer}>
+      <Text style={styles.fallbackText}>No places added yet. Start adding places.</Text>
+    </View>
+  }
+  return <FlatList
+    data={places}
+    keyExtractor={(item) => item.id}
+    renderItem={({ item }) => <PlaceItem place={item} />}
+  />
+}
+
+export default PlacesList;
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  fallbackText: {
+    fontSize: 16
+  }
+})
