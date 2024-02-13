db0d533f75c01a735ace1db71c030e0588ad4689
Wasim Hyder <wasim@creativefaze.com>
2024-02-13 21:51:47 +0500

    Added nav button

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import { FlatList, View, Text, StyleSheet } from 'react-native';
 import PlaceItem from './PlaceItem';
+import { Colors } from '../../constants/colors';
 
 function PlacesList({ places }) {
   if (!places || places.length === 0) {
@@ -23,6 +24,7 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   fallbackText: {
-    fontSize: 16
+    fontSize: 16,
+    color: Colors.primary200
   }
 })
