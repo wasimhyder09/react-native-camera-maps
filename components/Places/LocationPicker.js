6575ff3069b4d753bd989647c6ec9ca35575d2ba
Wasim Hyder <wasim@creativefaze.com>
2024-02-14 22:45:40 +0500

    Got user's live location

---
+++ components/Places/LocationPicker.js
@@ -0,0 +1,58 @@
+import { View, StyleSheet, Alert } from 'react-native';
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location';
+
+import OutlinedButton from '../UI/OutlinedButton';
+import { Colors } from '../../constants/colors';
+
+function LocationPicker() {
+  const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert('Insufficient permission', 'You need to grant location permissions to use this app.');
+      return false;
+    }
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
+  function pickOnMapHandler() { }
+  return <View>
+    <View style={styles.mapPreview}></View>
+    <View style={styles.actions}>
+      <OutlinedButton icon='location' onPress={getLocationHandler}>Locate User</OutlinedButton>
+      <OutlinedButton icon='map' onPress={pickOnMapHandler}>Open Map</OutlinedButton>
+    </View>
+  </View>
+}
+
+export default LocationPicker;
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+});
--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { ScrollView, View, Text, TextInput, StyleSheet } from 'react-native';
 import { Colors } from '../../constants/colors';
 import ImagePicker from './ImagePicker';
+import LocationPicker from './LocationPicker';
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState('');
 
@@ -15,6 +16,7 @@ function PlaceForm() {
       <TextInput style={styles.input} onChangeText={changeTitleHandler} value={enteredTitle} />
     </View>
     <ImagePicker />
+    <LocationPicker />
   </ScrollView>
 }
 
