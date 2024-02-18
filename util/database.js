09debbb477237032b886ec6ae99e9385036fbdb0
Wasim Hyder <wasim@creativefaze.com>
2024-02-18 18:48:21 +0500

    Saved data into database

--- util/database.js
+++ util/database.js
@@ -26,3 +26,26 @@ export function init() {
   });
   return promise;
 }
+
+export function insertPlace(place) {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(`INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+        [
+          place.title,
+          place.imageUri,
+          place.address,
+          place.location.lat,
+          place.location.lng
+        ],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, error) => {
+          reject(error);
+        }
+      );
+    });
+  });
+  return promise;
+}
